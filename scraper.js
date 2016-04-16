c5cc0e165dce025c6c6fb6a159dba2e7c1a48811
sudiptokarmoker <sudiptocsi@gmail.com>
2016-04-16 22:26:34 +0600

    Added files via upload

---
+++ scraper.js
@@ -0,0 +1,26 @@
+var webserver = require('webserver');
+var server = webserver.create();
+var webPage = require('webpage');
+var page = webPage.create();
+var port = require('system').env.PORT || 8080; // default back to 8080
+var service = server.listen(port, function(request, response) {
+  response.statusCode = 200; 
+  var str = request.url;
+    var url_to_scrap = str.split("/?url=");
+	page.open(url_to_scrap[1], function (status) {
+		if(status == "success"){
+			var content = page.content;
+			response.write(content);
+			response.close();
+		} 
+		else{
+			response.write("fail");
+			response.close();
+		}
+	});
+	// page or error handler //
+	page.onError = function(msg, trace){
+		response.write("fail");
+		response.close();
+	}
+});
