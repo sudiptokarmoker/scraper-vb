0d74fbfc8c0bed980c7d968c30472ee7b9abf545
sudiptokarmoker <sudiptocsi@gmail.com>
2016-04-23 18:59:09 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -7,8 +7,14 @@ var service = server.listen(port, function(request, response) {
   response.statusCode = 200; 
   var str = request.url;
     var url_to_scrap = str.split("/?url=");
+    page.settings.userAgent = 'SpecialAgent';
 	page.open(url_to_scrap[1], function (status) {
-		if(status == "success"){
+		page.evaluate(function() {
+		      var content = page.content;
+			response.write(content);
+			response.close();
+	        });
+		/*if(status == "success"){
 			var content = page.content;
 			response.write(content);
 			response.close();
@@ -16,7 +22,7 @@ var service = server.listen(port, function(request, response) {
 		else{
 			response.write("fail");
 			response.close();
-		}
+		}*/
 	});
 	// page or error handler //
 	page.onError = function(msg, trace){
