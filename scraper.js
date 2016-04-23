f2eda319bb8840ccc85273def80887b87838f190
sudiptokarmoker <sudiptocsi@gmail.com>
2016-04-23 19:06:07 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -7,14 +7,14 @@ var service = server.listen(port, function(request, response) {
   response.statusCode = 200; 
   var str = request.url;
     var url_to_scrap = str.split("/?url=");
-    page.settings.userAgent = 'SpecialAgent';
+    //page.settings.userAgent = 'SpecialAgent';
 	page.open(url_to_scrap[1], function (status) {
-		page.evaluate(function() {
+		/*page.evaluate(function() {
 		      var content = page.content;
 			response.write(content);
 			response.close();
-	        });
-		/*if(status == "success"){
+	        });*/
+		if(status == "success"){
 			var content = page.content;
 			response.write(content);
 			response.close();
@@ -22,7 +22,7 @@ var service = server.listen(port, function(request, response) {
 		else{
 			response.write("fail");
 			response.close();
-		}*/
+		}
 	});
 	// page or error handler //
 	page.onError = function(msg, trace){
