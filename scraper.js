8b0032a2a6936c094e79b5b489b27c7fe2a86419
sudiptokarmoker <sudiptocsi@gmail.com>
2016-04-23 17:38:50 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -7,6 +7,7 @@ var service = server.listen(port, function(request, response) {
   response.statusCode = 200; 
   var str = request.url;
     var url_to_scrap = str.split("/?url=");
+	/*
 	page.open(url_to_scrap[1], function (status) {
 		if(status == "success"){
 			var content = page.content;
@@ -18,6 +19,31 @@ var service = server.listen(port, function(request, response) {
 			response.close();
 		}
 	});
+	*/
+	
+	
+	page.open(url_to_scrap[1], function (status) { 
+		page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', function() { 
+			page.evaluate(function(config){ 
+				window.setTimeout(function(){
+					if(status == "success"){
+						var content = page.content;
+						response.write(content);
+						response.close();
+					} 
+					else{
+						response.write("fail");
+						response.close();
+					}
+				}, 1); 
+			});
+		}); 
+	});
+	
+	
+	
+	
+	
 	// page or error handler //
 	page.onError = function(msg, trace){
 		response.write("fail");
