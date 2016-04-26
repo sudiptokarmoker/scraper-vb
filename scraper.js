51ef38baa08097a0928eac2f732ad0b7db4a3080
sudiptokarmoker <sudiptocsi@gmail.com>
2016-04-26 15:11:06 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -4,16 +4,10 @@ var webPage = require('webpage');
 var page = webPage.create();
 var port = require('system').env.PORT || 8080; // default back to 8080
 var service = server.listen(port, function(request, response) {
-  response.statusCode = 200; 
-  var str = request.url;
-    var url_to_scrap = str.split("/?url=");
-    //page.settings.userAgent = 'SpecialAgent';
+	response.statusCode = 200; 
+	var str = request.url;
+	var url_to_scrap = str.split("/?url="); 
 	page.open(url_to_scrap[1], function (status) {
-		/*page.evaluate(function() {
-		      var content = page.content;
-			response.write(content);
-			response.close();
-	        });*/
 		if(status == "success"){
 			var content = page.content;
 			response.write(content);
