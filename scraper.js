bffafd41e381661bbd23700d4ca55ca35120dfc9
sudiptokarmoker <sudiptocsi@gmail.com>
2016-09-18 09:56:56 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -7,6 +7,24 @@ var service = server.listen(port, function(request, response) {
 	response.statusCode = 200; 
 	var str = request.url;
 	var url_to_scrap = str.split("/?url="); 
+	
+	
+	// advance for ensure that page is laodded //
+	page.onInitialized = function() {
+	    page.onCallback = function(data) {
+	        console.log('Main page is loaded and ready');
+	        //Do whatever here
+	    }; 
+	    page.evaluate(function() {
+	        document.addEventListener('DOMContentLoaded', function() {
+	            window.callPhantom();
+	        }, false);
+	        console.log("Added listener to wait for page ready");
+	    }); 
+	};
+	// end of advance check //
+	
+	
 	page.open(url_to_scrap[1], function (status) {
 		if(status == "success"){
 			var content = page.content;
