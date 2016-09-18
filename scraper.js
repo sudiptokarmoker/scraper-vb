610b429e39b98be660d09bbec0de6e3feacd509f
sudiptokarmoker <sudiptocsi@gmail.com>
2016-09-18 09:59:51 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -8,22 +8,34 @@ var service = server.listen(port, function(request, response) {
 	var str = request.url;
 	var url_to_scrap = str.split("/?url="); 
 	
-	
-	// advance for ensure that page is laodded //
-	page.onInitialized = function() {
-	    page.onCallback = function(data) {
-	        console.log('Main page is loaded and ready');
-	        //Do whatever here
-	    }; 
-	    page.evaluate(function() {
-	        document.addEventListener('DOMContentLoaded', function() {
-	            window.callPhantom();
-	        }, false);
-	        console.log("Added listener to wait for page ready");
-	    }); 
-	};
-	// end of advance check //
-	
+	page.open(url_to_scrap[1], function (status) {
+		if(status == "success"){
+			var content = page.content;
+			response.write(content);
+			response.close();
+		} 
+		else{
+			response.write("fail");
+			response.close();
+		}
+	});
+	// page or error handler //
+	page.onError = function(msg, trace){
+		response.write("fail");
+		response.close();
+	}
+});
+
+/*
+var webserver = require('webserver');
+var server = webserver.create();
+var webPage = require('webpage');
+var page = webPage.create();
+var port = require('system').env.PORT || 8080; // default back to 8080
+var service = server.listen(port, function(request, response) {
+	response.statusCode = 200; 
+	var str = request.url;
+	var url_to_scrap = str.split("/?url="); 
 	
 	page.open(url_to_scrap[1], function (status) {
 		if(status == "success"){
@@ -42,3 +54,5 @@ var service = server.listen(port, function(request, response) {
 		response.close();
 	}
 });
+
+*/
