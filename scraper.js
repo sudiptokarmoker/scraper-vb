608c5223524008dbce6e2bb48c2ae33a4beee07a
sudiptokarmoker <sudiptocsi@gmail.com>
2016-09-18 12:42:17 +0600

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,13 +1,59 @@
 var webserver = require('webserver');
 var server = webserver.create();
-var webPage = require('webpage');
-var page = webPage.create();
 var port = require('system').env.PORT || 8080; // default back to 8080
+
 var service = server.listen(port, function(request, response) {
-	response.statusCode = 200; 
+	//response.statusCode = 200; 
+	var webPage = require('webpage');
+	var page = webPage.create(), count = 0, forcedRenderTimeout, renderTimeout;
 	var str = request.url;
-	var url_to_scrap = str.split("/?url="); 
+	//var url_to_scrap = str.split("/?url="); 
+	var resourceWait  = 300, maxRenderWait = 10000, url_to_scrap = str.split("/?url=");
+	 
+	 
+	function doRender() {
+		var content = page.content;
+		response.write(content);
+		response.close();
+		//console.log(content); 
+		//page.render('twitter.png');
+		phantom.exit();
+	}
+
+	page.onResourceRequested = function (req) {
+		count += 1;
+		//console.log('> ' + req.id + ' - ' + req.url);
+		clearTimeout(renderTimeout);
+	};
+
+	page.onResourceReceived = function (res) {
+		if (!res.stage || res.stage === 'end') {
+			count -= 1;
+			//console.log(res.id + ' ' + res.status + ' - ' + res.url);
+			if (count === 0) {
+				renderTimeout = setTimeout(doRender, resourceWait);
+			}
+		}
+	};
+
+	page.open(url, function (status) {
+		if (status !== "success") {
+			//console.log('Unable to load url');
+			response.write("fail");
+			response.close();
+			phantom.exit();
+		} else {
+			forcedRenderTimeout = setTimeout(function () {
+				//console.log(count);
+				doRender();
+			}, maxRenderWait);
+		}
+	});
 	
+	
+	
+	
+	/*
 	page.open(url_to_scrap[1], function (status) {
 		if(status == "success"){
 			var content = page.content;
@@ -23,5 +69,5 @@ var service = server.listen(port, function(request, response) {
 	page.onError = function(msg, trace){
 		response.write("fail");
 		response.close();
-	}
+	}*/
 });
